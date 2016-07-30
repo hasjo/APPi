bca99ebdb18c4b776524b9a63365f3896dd838fc
Jordan Haskovec <jorhaskovec@gmail.com>
2016-07-29 22:05:36 -0500

    finished commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # APPi
 A flask API to get x digits of pi
+
+It rounds up, so beware
---
+++ Site.py
@@ -0,0 +1,25 @@
+'''This is my Flask Blog Website!'''
+from flask import Flask, render_template
+import os
+from mpmath import *
+from decimal import *
+APP = Flask(__name__)
+
+@APP.route('/')
+def index():
+    '''Returns the index template'''
+    return('curl this site /number to get number digits of pi, ex: site/4 will get 3.142')
+
+
+@APP.route('/<number>')
+def calc(number):
+    '''Generates the pi'''
+    try:
+        dignum = int(number)
+    except ValueError:
+        return "THATS NOT A NUMBER - TRY AGAIN"
+    mp.dps = dignum
+    return str(+pi)
+
+if __name__ == '__main__':
+    APP.run(debug=True, host='0.0.0.0', port=8080)
