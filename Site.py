286dfb5059b2c8c74f308bb0b11924e19d00d420
Jordan Haskovec <jorhaskovec@gmail.com>
2016-08-02 21:12:48 -0500

    rewording things

--- Site.py
+++ Site.py
@@ -1,4 +1,4 @@
-'''This is my Flask Blog Website!'''
+'''This is my Pi API Website!'''
 from flask import Flask, render_template
 import os
 from mpmath import *
@@ -7,7 +7,7 @@ APP = Flask(__name__)
 
 @APP.route('/')
 def index():
-    '''Returns the index template'''
+    '''Returns info stuff'''
     return('curl this site /number to get number digits of pi, ex: site/4 will get 3.142')
 
 
@@ -17,7 +17,7 @@ def calc(number):
     try:
         dignum = int(number)
     except ValueError:
-        return "THATS NOT A NUMBER - TRY AGAIN"
+        return "THATS NOT A WHOLE NUMBER - TRY AGAIN"
     mp.dps = dignum
     return str(+pi)
 
